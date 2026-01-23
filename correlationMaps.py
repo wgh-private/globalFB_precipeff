d7f524b3e3bb6b4e9a2f6c2e04b14a22bba944c6
wgh-private <geethma.wgh@gmail.com>
2026-01-23 03:55:49 -0700

    Plot correlation of global precipitation eff with ∆LWP and lambda scaled by PD P-E in maps without statistical significance

--- correlationMaps.py
+++ correlationMaps.py
@@ -89,115 +89,171 @@ SWcld_tot_fb_global = trav_fbvar('SWcld_tot', region_dic)
 totdCRE_g = LWcld_tot_fb_global['global'] + SWcld_tot_fb_global['global']
 totdCRE_g = totdCRE_g.sel(runs=common_members)
 
-
-
 PE_g_mean = PE_g_mean.sel(runs=common_members) 
 grad = [PE_g_mean.data]
 grad = np.array(grad)
 
+# -------------- Plot correlation of global precipitation eff with ∆LWP and lambda scaled by PD P-E in maps ----------------- 
+totdCRE_g_t = np.transpose(totdCRE_g.data, (0, 2, 1))
+dVAR_map_o = [dLWP_g.data * pe_maps_ol.data, totdCRE_g_t * pe_maps_ol.data]
+dVAR_map_o = np.array(dVAR_map_o)
+dVar_name = ['∆LWP/∆GMT * PD P-E', '$λ_{cld}$ * PD P-E']
 
-# lat_sh = dLWP_g.lat.shape[0]
-# lon_sh = dLWP_g.lon.shape[0]
+grad = [PE_g_mean.data]
+grad = np.array(grad)
+grad_name = ['P/LWP']
+
+# Set figure width to 11.4 cm = 4.49 inches for PNAS single-column
+fig = plt.figure(figsize=(4.49, 2.8))  # Adjusted height to maintain horizontal layout
+
+lat_sh = dLWP_g.lat.shape[0]
+lon_sh = dLWP_g.lon.shape[0]
 
 nums = [[0, 0], [0, 1]]
-title_num = ['(a) ', '(b) ', '(c) ']
-# dVar_name = ['∆LWP/∆GMT', '$λ_{cld}$']
-# statistical significance from Travis
-dir = '/glade/derecho/scratch/travisa/Statistical_testing_for_Geethma/'
-stipple = ['dPLWP_ddelLWP_statistical_test_0.3alph_1.nc', 'gpe_totcld_statistical_test_0.3alph.nc']
-# correlations from my saved npz
-sdir = "/glade/u/home/geethma/phd_research_home/globalFB/data/gpe_totcre_correlation_pvalue_maps.npz"
-print(sdir)
-data = np.load(sdir)
-lon, lat, corr_00, pval_00, corr_01, pval_01 = (
-    data["lon"],  #longitude
-    data["lat"],  #latitude
-    data["correlation_0_0"],  #correlation of P/LWP with ∆LWP
-    data["pvalue_0_0"],  #p-value of P/LWP with ∆LWP
-    data["correlation_0_1"],  #correlation of P/LWP with λCRE
-    data["pvalue_0_1"],  #p-value of P/LWP with λCRE
-)
-sdir = "/glade/u/home/geethma/phd_research_home/globalFB/data/gpe_totcld_correlation_pvalue_maps.npz"
-print(sdir)
-data = np.load(sdir)
-cld_correlation = data["correlation"]
-corrlist = [corr_00, cld_correlation]
+title_num = ['(a) ', '(b) ']
 
-# -------------- Plot correlation of global precipitation eff with ∆LWP and lambda scaled by PD P-E in maps ----------------- 
-fig, axs = plt.subplots(2, 1, figsize=(4.49, 5), subplot_kw={'projection': ccrs.Robinson()}, constrained_layout=True)  # Adjusted height to maintain vertical layout: horizontal layout:(4.49, 2.8))
+# PNAS-compliant font sizes
+title_fontsize = 8
+label_fontsize = 7
+tick_fontsize = 6
+
+fig, axs = plt.subplots(2, 1, figsize=(4.49, 5), subplot_kw={'projection': ccrs.Robinson()}, constrained_layout=True)
 
 for ax_n, ax in enumerate(axs.flat):
-    ax.set_facecolor("darkgrey")
+    print(dVar_name[ax_n])
     g = nums[ax_n][0]
     d = nums[ax_n][1]
-    
-    correlation = corrlist[ax_n]
-    stipdata = xr.open_dataset(dir+stipple[ax_n])
-    stipcorr = stipdata['Wilks_binary']*correlation
 
-    cbar_img = ax.pcolormesh(lon, lat, stipcorr,
+    correlation = np.full((lat_sh, lon_sh), np.nan)
+    for i in range(lat_sh):
+        for j in range(lon_sh):
+            correlation[i, j] = stats.spearmanr(grad[g], dVAR_map_o[d, :, i, j], nan_policy='omit')[0]
+
+    cbar_img = ax.pcolormesh(dLWP_g.lon, dLWP_g.lat, correlation,
                              transform=ccrs.PlateCarree(), cmap='seismic',
                              vmin=-1, vmax=1)
 
     ax.add_feature(cfeature.LAND, facecolor='white')
     ax.add_feature(cfeature.GSHHSFeature(scale='auto', edgecolor='black'))
     ax.set_extent([-180, 180, -90, 90], ccrs.PlateCarree())
-    ax.set_title(title_num[ax_n] + 'Global mean ' + grad_name[g] + ' correlation with ' + dVar_name[d],
+    ax.set_title(title_num[ax_n] + 'Global mean ' + grad_name[g] + ' correlation\nwith ' + dVar_name[d],
                  fontsize=title_fontsize, loc='left')
     gl = ax.gridlines(draw_labels=True, linewidth=0.5, color='black', alpha=0.8, linestyle='--')
     gl.top_labels = False
     gl.right_labels = False
-    gl.xlabel_style = {'fontsize': label_fontsize}
+    gl.xlabel_style = {'fontsize': label_fontsize}  # optional: set label font size
     gl.ylabel_style = {'fontsize': label_fontsize}
 
-# Single horizontal colorbar below
 cbar = fig.colorbar(cbar_img, ax=axs, orientation='vertical', fraction=0.05, pad=0.07)
 cbar.set_label('Correlation', fontsize=label_fontsize)
 cbar.ax.tick_params(labelsize=tick_fontsize)
 
-plt.savefig(fig_dir+'gPE_λcld_∆LWP_correlation_statistical.png',
+plt.savefig(f'{fig_dir}gPE_λcldpdPE_∆LWPpdPE_correlation.png',
             bbox_inches='tight', facecolor='white', dpi=600)
 
-# -------------- Plot ∆LWP/∆GMT, lambda, and PD P-E in maps ----------------- 
-lon = dLWP_g.lon
-lat = dLWP_g.lat
-dVAR_map_o = [dLWP_g[0].data, np.transpose(totdCRE_g[0].data), pe_maps_ol[0].data]
-dVar_name = ['∆LWP/∆GMT', '$λ_{cld}$', 'PD P-E']
-fig, axs = plt.subplots(3, 1, figsize=(4.49, 9), subplot_kw={'projection': ccrs.Robinson()}, constrained_layout=True)  # Adjusted height to maintain vertical layout: horizontal layout:(4.49, 2.8))
 
-for ax_n, ax in enumerate(axs.flat):
-    print(dVar_name[ax_n])
-    ax.set_facecolor("darkgrey")
-    # g = nums[0][ax_n]
-    # d = nums[0][ax_n]
-    plot_var = dVAR_map_o[ax_n]
-    cbar_img = ax.pcolormesh(lon, lat, plot_var,
-                             transform=ccrs.PlateCarree(), cmap='seismic')
 
-    ax.add_feature(cfeature.LAND, facecolor='white')
-    ax.add_feature(cfeature.GSHHSFeature(scale='auto', edgecolor='black'))
-    ax.set_extent([-180, 180, -90, 90], ccrs.PlateCarree())
-    ax.set_title(title_num[ax_n] + f'{dVar_name[ax_n]} for the CAM6 default run',
-                 fontsize=title_fontsize, loc='left')
-    gl = ax.gridlines(draw_labels=True, linewidth=0.5, color='black', alpha=0.8, linestyle='--')
-    gl.top_labels = False
-    gl.right_labels = False
-    gl.xlabel_style = {'fontsize': label_fontsize}
-    gl.ylabel_style = {'fontsize': label_fontsize}
 
-    cbar = fig.colorbar(
-        cbar_img,
-        ax=ax,
-        orientation='horizontal',
-        fraction=0.05,
-        pad=0.07
-    )
-    cbar.set_label(dVar_name[ax_n], fontsize=label_fontsize)
-    cbar.ax.tick_params(labelsize=tick_fontsize)
-    max_abs_value = np.nanmax(np.abs(plot_var))
-    cbar_img.set_clim(-max_abs_value, max_abs_value)
+# # -------------- Plot correlation of global precipitation eff with ∆LWP and lambda in maps ----------------- 
 
-plt.savefig(fig_dir+'λcld_pdPE_∆LWP_maps.png',
-            bbox_inches='tight', facecolor='white', dpi=600)
+# nums = [[0, 0], [0, 1]]
+# title_num = ['(a) ', '(b) ', '(c) ']
+# # dVar_name = ['∆LWP/∆GMT', '$λ_{cld}$']
+# # statistical significance from Travis
+# dir = '/glade/derecho/scratch/travisa/Statistical_testing_for_Geethma/'
+# stipple = ['dPLWP_ddelLWP_statistical_test_0.3alph_1.nc', 'gpe_totcld_statistical_test_0.3alph.nc']
+# # correlations from my saved npz
+# sdir = "/glade/u/home/geethma/phd_research_home/globalFB/data/gpe_totcre_correlation_pvalue_maps.npz"
+# print(sdir)
+# data = np.load(sdir)
+# lon, lat, corr_00, pval_00, corr_01, pval_01 = (
+#     data["lon"],  #longitude
+#     data["lat"],  #latitude
+#     data["correlation_0_0"],  #correlation of P/LWP with ∆LWP
+#     data["pvalue_0_0"],  #p-value of P/LWP with ∆LWP
+#     data["correlation_0_1"],  #correlation of P/LWP with λCRE
+#     data["pvalue_0_1"],  #p-value of P/LWP with λCRE
+# )
+# sdir = "/glade/u/home/geethma/phd_research_home/globalFB/data/gpe_totcld_correlation_pvalue_maps.npz"
+# print(sdir)
+# data = np.load(sdir)
+# cld_correlation = data["correlation"]
+# corrlist = [corr_00, cld_correlation]
+
+# fig, axs = plt.subplots(2, 1, figsize=(4.49, 5), subplot_kw={'projection': ccrs.Robinson()}, constrained_layout=True)  # Adjusted height to maintain vertical layout: horizontal layout:(4.49, 2.8))
+
+# for ax_n, ax in enumerate(axs.flat):
+#     ax.set_facecolor("darkgrey")
+#     g = nums[ax_n][0]
+#     d = nums[ax_n][1]
+    
+#     correlation = corrlist[ax_n]
+#     stipdata = xr.open_dataset(dir+stipple[ax_n])
+#     stipcorr = stipdata['Wilks_binary']*correlation
+
+#     cbar_img = ax.pcolormesh(lon, lat, stipcorr,
+#                              transform=ccrs.PlateCarree(), cmap='seismic',
+#                              vmin=-1, vmax=1)
+
+#     ax.add_feature(cfeature.LAND, facecolor='white')
+#     ax.add_feature(cfeature.GSHHSFeature(scale='auto', edgecolor='black'))
+#     ax.set_extent([-180, 180, -90, 90], ccrs.PlateCarree())
+#     ax.set_title(title_num[ax_n] + 'Global mean ' + grad_name[g] + ' correlation with ' + dVar_name[d],
+#                  fontsize=title_fontsize, loc='left')
+#     gl = ax.gridlines(draw_labels=True, linewidth=0.5, color='black', alpha=0.8, linestyle='--')
+#     gl.top_labels = False
+#     gl.right_labels = False
+#     gl.xlabel_style = {'fontsize': label_fontsize}
+#     gl.ylabel_style = {'fontsize': label_fontsize}
+
+# # Single horizontal colorbar below
+# cbar = fig.colorbar(cbar_img, ax=axs, orientation='vertical', fraction=0.05, pad=0.07)
+# cbar.set_label('Correlation', fontsize=label_fontsize)
+# cbar.ax.tick_params(labelsize=tick_fontsize)
+
+# plt.savefig(fig_dir+'gPE_λcld_∆LWP_correlation_statistical.png',
+#             bbox_inches='tight', facecolor='white', dpi=600)
+
+# # -------------- Plot ∆LWP/∆GMT, lambda, and PD P-E in maps ----------------- 
+# lon = dLWP_g.lon
+# lat = dLWP_g.lat
+# dVAR_map_o = [dLWP_g[0].data, np.transpose(totdCRE_g[0].data), pe_maps_ol[0].data]
+# dVar_name = ['∆LWP/∆GMT', '$λ_{cld}$', 'PD P-E']
+# fig, axs = plt.subplots(3, 1, figsize=(4.49, 9), subplot_kw={'projection': ccrs.Robinson()}, constrained_layout=True)  # Adjusted height to maintain vertical layout: horizontal layout:(4.49, 2.8))
+
+# for ax_n, ax in enumerate(axs.flat):
+#     print(dVar_name[ax_n])
+#     ax.set_facecolor("darkgrey")
+#     # g = nums[0][ax_n]
+#     # d = nums[0][ax_n]
+#     plot_var = dVAR_map_o[ax_n]
+#     cbar_img = ax.pcolormesh(lon, lat, plot_var,
+#                              transform=ccrs.PlateCarree(), cmap='seismic')
+
+#     ax.add_feature(cfeature.LAND, facecolor='white')
+#     ax.add_feature(cfeature.GSHHSFeature(scale='auto', edgecolor='black'))
+#     ax.set_extent([-180, 180, -90, 90], ccrs.PlateCarree())
+#     ax.set_title(title_num[ax_n] + f'{dVar_name[ax_n]} for the CAM6 default run',
+#                  fontsize=title_fontsize, loc='left')
+#     gl = ax.gridlines(draw_labels=True, linewidth=0.5, color='black', alpha=0.8, linestyle='--')
+#     gl.top_labels = False
+#     gl.right_labels = False
+#     gl.xlabel_style = {'fontsize': label_fontsize}
+#     gl.ylabel_style = {'fontsize': label_fontsize}
+
+#     cbar = fig.colorbar(
+#         cbar_img,
+#         ax=ax,
+#         orientation='horizontal',
+#         fraction=0.05,
+#         pad=0.07
+#     )
+#     cbar.set_label(dVar_name[ax_n], fontsize=label_fontsize)
+#     cbar.ax.tick_params(labelsize=tick_fontsize)
+#     max_abs_value = np.nanmax(np.abs(plot_var))
+#     cbar_img.set_clim(-max_abs_value, max_abs_value)
+
+# plt.savefig(fig_dir+'λcld_pdPE_∆LWP_maps.png',
+#             bbox_inches='tight', facecolor='white', dpi=600)
 
 
